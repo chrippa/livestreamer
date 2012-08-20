d3b501c355083ce08abc243b259f19f4e18dbfc2
Niall McAndrew <niallm90@gmail.com>
2012-08-21 06:26:55 +1200

    Added PrettyTables as a dependancy

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 from sys import version_info, platform, prefix
 
 version = "1.1.0"
-deps = ["pbs"]
+deps = ["pbs", "PrettyTable"]
 
 # require argparse on Python <2.7 and <3.2
 if (version_info[0] == 2 and version_info[1] < 7) or \
