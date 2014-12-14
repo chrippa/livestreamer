7466053b0cda065fd5f4e0e53ece632eb9a5f5be
cybersphinx <chr.ohm@gmx.net>
2014-12-14 08:38:01 +0100

    plugins.youtube: Support Python 3 as well.

--- src/livestreamer/plugins/youtube.py
+++ src/livestreamer/plugins/youtube.py
@@ -1,5 +1,5 @@
 import re
-import urllib
+from livestreamer.compat import unquote
 
 from livestreamer.plugin import Plugin, PluginError
 from livestreamer.plugin.api import http, validate
@@ -108,7 +108,7 @@ _url_re = re.compile("""
 class YouTube(Plugin):
     @classmethod
     def can_handle_url(self, url):
-        return _url_re.match(urllib.unquote(url))
+        return _url_re.match(unquote(url))
 
     @classmethod
     def stream_weight(cls, stream):
@@ -144,7 +144,7 @@ class YouTube(Plugin):
             return video_id
 
     def _get_stream_info(self, url):
-        match = _url_re.match(urllib.unquote(url))
+        match = _url_re.match(unquote(url))
         user = match.group("user")
 
         if user:
