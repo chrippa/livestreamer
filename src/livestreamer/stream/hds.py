569bd4bbedbffae9be30997b33f1f4ba0ff41278
Benoît Laurent <blaurent@edd.fr>
2015-03-18 15:12:35 +0100

    Try to fix problem with dailymotion live stream.
    This patch remove trailling parameter in the media url found in f4m manifest.
    
    See chrippa/livestreamer#786 for more details.

--- src/livestreamer/stream/hds.py
+++ src/livestreamer/stream/hds.py
@@ -464,7 +464,11 @@ class HDSStream(Stream):
             request_params["params"].update(params)
 
         for media in manifest.findall("media"):
+            import pdb ; pdb.set_trace()
             url = media.attrib.get("url")
+            # Remove potentially present parameter in media url that break
+            # fragment url construction ; Example ?hls_no_audio=1
+            url = url.split('?')[0]
             bootstrapid = media.attrib.get("bootstrapInfoId", "_global")
             href = media.attrib.get("href")
 
