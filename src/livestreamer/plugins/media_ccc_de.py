933eb327850d08ba84f4107bbbd0829cd90f2ebb
Daniel Meißner <meise@koeln.ccc.de>
2015-04-16 12:29:37 +0200

    plugin: added chrippa's suggestions to media_ccc_de plugin
    * removed requests and json imports
    * removed usage of .keys() function
    * use .items() in for loops to have key and value directly assigned
    * removed exception handling for HLSStream
    * replaced .search with .match to catch handled URLs

--- src/livestreamer/plugins/media_ccc_de.py
+++ src/livestreamer/plugins/media_ccc_de.py
@@ -20,10 +20,9 @@ Limitations:
 """
 
 import re
-import requests
-import json
 
 from livestreamer.plugin import Plugin, PluginError
+from livestreamer.plugin.api import http
 from livestreamer.stream import HTTPStream, HLSStream
 
 API_URL_MEDIA           = "https://api.media.ccc.de"
@@ -33,14 +32,14 @@ API_URL_STREAMING_MEDIA = "http://streaming.media.ccc.de/streams/v1.json"
 _url_media_re           = re.compile("(?P<scheme>http|https)"
                                      ":\/\/"
                                      "(?P<server>media\.ccc\.de)"
-                                     "\/")
+                                     "\/.*\.html")
 # http://streaming.media.ccc.de/room/
 _url_streaming_media_re = re.compile("(?P<scheme>http)"
                                      ":\/\/"
                                      "(?P<server>streaming\.media\.ccc\.de)"
                                      "\/"
                                      "(?P<room>.*)"
-                                     "\/.*")
+                                     "\/")
 
 def get_event_id(url):
     """Extract event id from talk html page.
@@ -50,7 +49,7 @@ def get_event_id(url):
     :param url: talk URL
 
     """
-    match = re.search(r"{event_id:\s(?P<event_id>\d+),.*}", get_page(url))
+    match = re.search(r"{event_id:\s(?P<event_id>\d+),.*}", http.get(url).text)
 
     try:
         event_id = int(match.group('event_id'))
@@ -59,39 +58,22 @@ def get_event_id(url):
 
     return event_id
 
-def get_page(url):
-    """Fetch page for given URL.
+def get_json(url):
+    """Fetch page for given URL and return json Python object.
 
     :param url: URL to fetch
 
     """
-    page = requests.get(url)
+    res = http.get(url)
 
-    return page.text
+    return http.json(res)
 
-def create_json_object(json_string):
-    """Loads json string and returned Python object.
-
-    Raises :exc:`PluginError` on failure.
+def parse_media_json(json_object):
+    """Expose available file formats.
 
     :param json_string: json as string
 
     """
-    try:
-        json_object = json.loads(json_string)
-    except:
-        raise PluginError("Could not parse json from API.")
-
-    return json_object
-
-def parse_media_json(json_string):
-    """Expose available
-
-    :param json_string: json as string
-
-    """
-    json_object = create_json_object(json_string)
-
     recordings = {}
     for recording in json_object['recordings']:
         match       = re.search(r".*\/(?P<format>.*)", recording['mime_type'])
@@ -116,7 +98,7 @@ def parse_media_json(json_string):
 
     return recordings
 
-def parse_streaming_media_json(json_string, room_from_url):
+def parse_streaming_media_json(json_object, room_from_url):
     """Filter all availabe live streams for given json and room name.
 
     API-Doku: https://github.com/voc/streaming-website#json-api
@@ -125,8 +107,6 @@ def parse_streaming_media_json(json_string, room_from_url):
     :param room_from_url:
 
     """
-    json_object = create_json_object(json_string)
-
     streams = {}
     for group in json_object:
         for room in group['rooms']:
@@ -143,31 +123,34 @@ def parse_streaming_media_json(json_string, room_from_url):
                     language = 'translated'
 
                 # get available hls stream urls
-                if 'hls' in stream['urls'].keys():
-                    stream_url = stream['urls']['hls']['url']
+                hls_stream = stream['urls'].get('hls')
+                if hls_stream:
+                    stream_url = hls_stream['url']
                     name = None
                     # native HLS streams are announced as
                     # ${height}p and (hd|sd)_native_${height}p
                     if language == 'native':
-                        name          = "%sp" % stream['videoSize'][-1]
-                        long_name     = "hls_%s_%sp" % ("native",\
-                                                        stream['videoSize'][-1])
+                        name      = "%sp" % stream['videoSize'][-1]
+                        long_name = "hls_%s_%sp" % ("native",\
+                                                    stream['videoSize'][-1])
                         streams[name]      = stream_url
                         streams[long_name] = stream_url
                     elif language == 'translated':
-                        long_name     = "hls_%s_%sp" % ("translated",\
-                                                        stream['videoSize'][-1])
+                        long_name = "hls_%s_%sp" % ("translated",\
+                                                    stream['videoSize'][-1])
                         streams[long_name] = stream_url
 
                 # get available audio only mpeg urls
-                if 'mp3' in stream['urls'].keys():
-                    stream_url    = stream['urls']['mp3']['url']
+                mp3_stream = stream['urls'].get('mp3')
+                if mp3_stream:
+                    stream_url    = mp3_stream['url']
                     name          = "audio_%s_mpeg" % language
                     streams[name] = stream_url
 
                 # get available audio only opus urls
-                if 'opus' in stream['urls'].keys():
-                    stream_url    = stream['urls']['opus']['url']
+                opus_stream = stream['urls'].get('opus')
+                if opus_stream:
+                    stream_url    = opus_stream['url']
                     name          = "audio_%s_opus" % language
                     streams[name] = stream_url
 
@@ -177,7 +160,7 @@ def parse_streaming_media_json(json_string, room_from_url):
 class media_ccc_de(Plugin):
     @classmethod
     def can_handle_url(self, url):
-        return _url_media_re.search(url) or _url_streaming_media_re.search(url)
+        return _url_media_re.match(url) or _url_streaming_media_re.match(url)
 
     def _get_streams(self):
         streams = {}
@@ -186,29 +169,24 @@ class media_ccc_de(Plugin):
         match = _url_streaming_media_re.match(self.url)
         if match:
             query_url    = API_URL_STREAMING_MEDIA
-            live_streams = parse_streaming_media_json(get_page(query_url),\
+            live_streams = parse_streaming_media_json(get_json(query_url),\
                                                       match.group('room'))
 
-            for stream_name in live_streams.keys():
+            for stream_name, stream_url in live_streams.items():
                 if re.search(r"m3u8", live_streams[stream_name]):
-                    try:
-                        streams[stream_name] = HLSStream(self.session,\
-                                                live_streams[stream_name])
-                    except IOError as err:
-                        self.logger.warning("Failed to extract HLS streams: "
-                                            "{0}", err)
+                    streams[stream_name] = HLSStream(self.session,\
+                                                        stream_url)
                 else:
                     streams[stream_name] = HTTPStream(self.session,\
-                                                live_streams[stream_name])
+                                                        stream_url)
 
         # media.ccc.de
         elif _url_media_re.search(self.url):
             event_id   = get_event_id(self.url)
             query_url  = "%s/public/events/%i" % (API_URL_MEDIA, event_id)
-            recordings = parse_media_json(get_page(query_url))
+            recordings = parse_media_json(get_json(query_url))
 
-            for name in recordings.keys():
-                stream_url    = recordings[name]
+            for name, stream_url in recordings.items():
                 streams[name] = HTTPStream(self.session, stream_url)
 
         if not streams:
