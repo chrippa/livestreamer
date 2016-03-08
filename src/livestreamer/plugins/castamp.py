8a8c8c3755b02ee817fbd1105de2ba103a0e9bf2
rbong <r.l.bongers@gmail.com>
2016-03-08 17:14:41 -0500

    Switched back to schema format.

--- src/livestreamer/plugins/castamp.py
+++ src/livestreamer/plugins/castamp.py
@@ -5,14 +5,41 @@ from livestreamer.plugin.api import http, validate
 from livestreamer.stream import RTMPStream
 
 EMBED_URL = "http://www.castamp.com/embed.php?c={}"
-VAR_RE = "var {} = '(.*)';"
 
 _url_re = re.compile("http(s)?://(\w+\.)?castamp.com/live/(?P<channel>[^/?]+)")
 _embed_re = re.compile("http(s)?://(\w+\.)?castamp.com/embed\.php\?c=(?P<channel>[^&]+)")
 _comment_re = re.compile("/\\*(.|[\\r\\n])*\\*/")
-_filename_re = re.compile("'file':\s*(?P<filename>'?.+'?),")
-_streamer_re = re.compile("'streamer':\s*(?P<streamer>'?.+'?),")
+_filename_re = re.compile("'file':\s*'(?P<filename>.+)',")
+_streamer_re = re.compile("'streamer':\s*'(?P<streamer>.+)',")
 _string_re = re.compile("^'(.*)'$")
+_var_re = re.compile("var\s+(?P<var>[^\s]+)\s+=\s+(?P<value>'.+')\s*;[\\n|\\r]")
+
+_schema = validate.Schema(
+    validate.transform(lambda x: _comment_re.sub("", x)),
+    validate.union({
+        "data": validate.text,
+        "var_matches": validate.transform(_var_re.findall)
+    }),
+)
+
+_filename_schema = validate.Schema(
+        validate.transform(_filename_re.search),
+        validate.any(
+            None,
+            validate.get("filename")
+        ),
+)
+
+_streamer_schema = validate.Schema(
+        validate.transform(_streamer_re.search),
+        validate.any(
+            None,
+            validate.all(
+                validate.get("streamer"),
+                validate.url(scheme="rtmp")
+            )
+        ),
+)
 
 class CastAmp(Plugin):
     @classmethod
@@ -23,35 +50,19 @@ class CastAmp(Plugin):
         channel = _url_re.match(self.url)
         if not channel: channel = _embed_re.match(self.url)
         channel = channel.group("channel")
+
         embed_url = EMBED_URL.format(channel)
-        res = http.get(embed_url)
+        res = http.get(embed_url, schema=_schema)
 
-        if not res or not res.text:
+        if not res or not res["data"]:
             return
-        res = res.text
-        res = _comment_re.sub ("", res)
+        for var_match in res["var_matches"]:
+            var, value = var_match
+            res["data"] = re.sub (var, value, res["data"])
 
-        filename = _filename_re.search(res)
-        if not filename: return
-        filename = filename.group('filename')
-        if not _string_re.search(filename):
-            _filename_var_re = re.compile(VAR_RE.format(filename))
-            filename = _filename_var_re.search(res)
-            if not filename: return
-            filename = filename.group(1)
-        else:
-            filename = _string_re.search(filename).group(1)
+        filename = _filename_schema.validate(res["data"])
 
-        streamer = _streamer_re.search(res)
-        if not streamer: return
-        streamer = streamer.group('streamer')
-        if not _string_re.search(streamer):
-            _streamer_var_re = re.compile(VAR_RE.format(streamer))
-            streamer = _streamer_var_re.search(res)
-            if not streamer: return
-            streamer = streamer.group(1)
-        else:
-            streamer = _string_re.search(streamer).group(1)
+        streamer = _streamer_schema.validate(res["data"])
 
         if not validate.startswith(channel)(filename):
             return
