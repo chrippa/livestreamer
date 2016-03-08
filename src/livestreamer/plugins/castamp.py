7b6933c788d0610cc0e47782b474f7188f19b111
rbong <r.l.bongers@gmail.com>
2016-03-07 22:31:58 -0500

    New castamp.com format.

--- src/livestreamer/plugins/castamp.py
+++ src/livestreamer/plugins/castamp.py
@@ -5,57 +5,64 @@ from livestreamer.plugin.api import http, validate
 from livestreamer.stream import RTMPStream
 
 EMBED_URL = "http://www.castamp.com/embed.php?c={}"
+VAR_RE = "var {} = '(.*)';"
 
 _url_re = re.compile("http(s)?://(\w+\.)?castamp.com/live/(?P<channel>[^/?]+)")
+_embed_re = re.compile("http(s)?://(\w+\.)?castamp.com/embed\.php\?c=(?P<channel>[^&]+)")
 _comment_re = re.compile("/\\*(.|[\\r\\n])*\\*/")
-_file_re = re.compile("'file':\s*'(?P<file>.+)',")
-_streamer_re = re.compile("'streamer':\s*'(?P<streamer>.+)',")
-
-_embed_schema = validate.Schema(
-    validate.transform(lambda x: _comment_re.sub("", x)),
-    validate.union({
-        "file": validate.all(
-            validate.transform(_file_re.search),
-            validate.any(
-                None,
-                validate.get("file")
-            ),
-        ),
-        "streamer": validate.all(
-            validate.transform(_streamer_re.search),
-            validate.any(
-                None,
-                validate.all(
-                    validate.get("streamer"),
-                    validate.url(scheme="rtmp")
-                )
-            ),
-        )
-    }),
-)
+_filename_re = re.compile("'file':\s*(?P<filename>'?.+'?),")
+_streamer_re = re.compile("'streamer':\s*(?P<streamer>'?.+'?),")
+_string_re = re.compile("^'(.*)'$")
 
 class CastAmp(Plugin):
     @classmethod
     def can_handle_url(cls, url):
-        return _url_re.match(url)
+        return _url_re.match(url) or _embed_re.match(url)
 
     def _get_streams(self):
         channel = _url_re.match(self.url).group("channel")
         embed_url = EMBED_URL.format(channel)
-        res = http.get(embed_url, schema=_embed_schema)
+        res = http.get(embed_url)
 
-        if not res["file"] or not res["streamer"]:
+        if not res or not res.text:
             return
-        if not validate.startswith(channel)(res["file"]):
+        res = res.text
+        res = _comment_re.sub ("", res)
+
+        filename = _filename_re.search(res)
+        if not filename: return
+        filename = filename.group('filename')
+        if not _string_re.search(filename):
+            _filename_var_re = re.compile(VAR_RE.format(filename))
+            filename = _filename_var_re.search(res)
+            if not filename: return
+            filename = filename.group(1)
+        else:
+            filename = _string_re.search(filename).group(1)
+
+        streamer = _streamer_re.search(res)
+        if not streamer: return
+        streamer = streamer.group('streamer')
+        if not _string_re.search(streamer):
+            _streamer_var_re = re.compile(VAR_RE.format(streamer))
+            streamer = _streamer_var_re.search(res)
+            if not streamer: return
+            streamer = streamer.group(1)
+        else:
+            streamer = _string_re.search(streamer).group(1)
+
+        if not validate.startswith(channel)(filename):
             return
 
-        rtmp_url = res["streamer"] + "/" + res["file"]
+        rtmp_url = streamer + "/" + filename
 
         stream = RTMPStream(self.session, {
             "rtmp": rtmp_url,
             "pageUrl": embed_url,
             "live": True,
         })
+
+        print (rtmp_url)
 
         return dict(live=stream)
 
