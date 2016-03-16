307f4cacc92d3bebe201aba63eff63fe3eb077d6
rbong <roger.bongers@yahoo.ca>
2016-03-16 13:38:47 +0000

    Added ability to parse javascript's unescape function.

--- src/livestreamer/plugins/castamp.py
+++ src/livestreamer/plugins/castamp.py
@@ -3,21 +3,27 @@ import re
 from livestreamer.plugin import Plugin
 from livestreamer.plugin.api import http, validate
 from livestreamer.stream import RTMPStream
+from livestreamer.utils import parse_json
 
 EMBED_URL = "http://www.castamp.com/embed.php?c={}"
 
-_url_re = re.compile("http(s)?://(\w+\.)?castamp.com/live/(?P<channel>[^/?]+)")
+_url_re = re.compile("http(s)?://(\w+\.)?castamp.com/(embed\.php\?c=|live/)(?P<channel>[^/?&]+)")
 _embed_re = re.compile("http(s)?://(\w+\.)?castamp.com/embed\.php\?c=(?P<channel>[^&]+)")
 _comment_re = re.compile("/\\*(.|[\\r\\n])*\\*/")
 _filename_re = re.compile("'file':\s*'(?P<filename>.+)',")
 _streamer_re = re.compile("'streamer':\s*'(?P<streamer>.+)',")
 _string_re = re.compile("^'(.*)'$")
+_unicode_re = re.compile("'\+unescape\('(?P<string>[^']*)'\)\+'")
 _var_re = re.compile("var\s+(?P<var>[^\s]+)\s+=\s+(?P<value>'.+')\s*;[\\n|\\r]")
 
 _schema = validate.Schema(
+    validate.text
+)
+
+_var_schema = validate.Schema(
     validate.transform(lambda x: _comment_re.sub("", x)),
     validate.union({
-        "data": validate.text,
+        "text": validate.text,
         "var_matches": validate.transform(_var_re.findall)
     }),
 )
@@ -44,25 +50,35 @@ _streamer_schema = validate.Schema(
 class CastAmp(Plugin):
     @classmethod
     def can_handle_url(cls, url):
-        return _url_re.match(url) or _embed_re.match(url)
+        return _url_re.match(url)
 
     def _get_streams(self):
-        channel = _url_re.match(self.url)
-        if not channel: channel = _embed_re.match(self.url)
-        channel = channel.group("channel")
+        channel = _url_re.match(self.url).group("channel")
 
         embed_url = EMBED_URL.format(channel)
         res = http.get(embed_url, schema=_schema)
 
-        if not res or not res["data"]:
+        if not res:
+            return
+
+        unicode_matches = _unicode_re.finditer(res);
+        for unicode_match in unicode_matches:
+            s = res [:unicode_match.start(0)]
+            s += re.sub('%', '\\x', unicode_match.group('string')).decode('unicode-escape')
+            s += res [unicode_match.end(0):]
+            res = s
+
+        res = _var_schema.validate(res)
+
+        if not res or not res["text"]:
             return
         for var_match in res["var_matches"]:
             var, value = var_match
-            res["data"] = re.sub (var, value, res["data"])
+            res["text"] = re.sub(var, value, res["text"])
 
-        filename = _filename_schema.validate(res["data"])
+        filename = _filename_schema.validate(res["text"])
 
-        streamer = _streamer_schema.validate(res["data"])
+        streamer = _streamer_schema.validate(res["text"])
 
         if not validate.startswith(channel)(filename):
             return
@@ -74,8 +90,6 @@ class CastAmp(Plugin):
             "pageUrl": embed_url,
             "live": True,
         })
-
-        print (rtmp_url)
 
         return dict(live=stream)
 
